d5eb045c5c8a30cc08c7bf38ddcd51b4eef4cfed
ccsandeepkumar <ccsandeepkumar@gmail.com>
2026-02-05 14:08:49 +0530

    Update test_sample.py

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -3,5 +3,9 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 def test_logging():
-    logging.info("This will show in logs")
+    logging.info("This will show in logs1")
+    logging.info("This will show in logs2")
+    logging.info("This will show in logs3")
+    logging.info("This will show in logs4")
+    logging.info("This will show in logs5")
     assert True
